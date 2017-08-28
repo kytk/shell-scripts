5b0f4a53bf57588b96d918a9eb07cd90bf7b20ac
kytk <kiyotaka@nemotos.net>
2017-08-28 16:51:58 +0900

    Add a script to replace spaces within file names or directory names with underscores

---
+++ replace-space-underscore.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# A script to substitute spaces within file/directory name with underscores.
+
+while true; do
+    echo "This script replaces spaces within file/directory name with underscores."
+    echo "Do you want to proceed (yes/no)?"
+
+    read answer
+
+    case $answer in
+        [Yy]*)
+            for i in $(seq $(find . -type d | wc -l))
+            do
+                find . -maxdepth $i -name '* *' | \
+                while read line
+                do newline=`echo $line | sed 's/ /_/g'`
+                    echo $newline
+                    mv "$line" $newline
+                done
+            done
+            break
+            ;;
+        [Nn]*)
+            echo "Abort the script."
+            break
+            ;;
+        *)
+            echo "Type yes or no."
+            ;;
+    esac
+done
+
+exit
+

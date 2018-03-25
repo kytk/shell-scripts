1c3035126cbd2a25f8b1ded510c9ade36bba79a7
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2018-03-25 12:40:01 +0900

    add cocoro_dicom_dcmtk using dcmdump

---
+++ cocoro_dicom_dcmtk.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+#Acquire DICOM header using dcmdump in dcmtk
+
+timestamp=$(date +%Y%m%d)
+timestamp2=$(date +%Y%m%d%H%M)
+
+summary=summary_${timestamp2}.txt
+
+dcmdump `ls | head -1 ` | sed -e 's/  *#.*$//' -e 's/^  *//' -e 's/^  *$//' | sed -e 's/=//g' -e 's/\[//' -e 's/\]//' > tmp
+
+echo "確認日:			${timestamp}" > $summary
+
+temp=$(grep '0008,0080' tmp | cut -c 16-)
+institution=${temp}
+echo "施設名:			${institution}" >> $summary
+
+temp=$(grep '0020,0011' tmp | cut -c 16-)
+seq_no=${temp}
+echo "シークエンス番号:	${seq_no}" >> $summary
+
+temp=$(grep '0008,0070' tmp | cut -c 16-)
+vendor=${temp}
+echo "メーカー:		${vendor}" >> $summary
+
+temp=$(grep '0008,1090' tmp | cut -c 16-)
+model=${temp}
+echo "機種名:			${model}" >> $summary
+
+temp=$(grep '0018,0087' tmp | cut -c 16-)
+mag_field_strength=${temp}
+echo "磁場強度:		${mag_field_strength}T" >> $summary
+
+temp=$(grep '0018,1020' tmp | cut -c 16-)
+software_ver=${temp}
+echo "システムバージョン:	${software_ver}" >> $summary
+
+temp=$(grep '0018,1250' tmp | cut -c 16-)
+head_coil=${temp}
+echo "Head_Coil:		N/A" >> $summary
+
+temp=$(grep '0008,103e' tmp | cut -c 16-)
+pulse_sequence=${temp}
+echo "pulse_sequence:		${pulse_sequence}" >> $summary
+
+#N/A	       	Acceleration mode
+echo "Acceleration_mode:	N/A" >> $summary
+
+temp=$(grep '0018,0080' tmp | cut -c 16-)
+tr=${temp}
+echo "TR:			${tr}" >> $summary
+
+temp=$(grep '0018,0081' tmp | cut -c 16-)
+te=${temp}
+echo "TE:			${te}" >> $summary
+
+temp=$(grep '0018,0082' tmp | cut -c 16-)
+ti=${temp}
+echo "TI:			${ti}" >> $summary
+
+temp=$(grep '0018,1314' tmp | cut -c 16-)
+flip_angle=${temp}
+echo "Flip_Angle:		${flip_angle}" >> $summary
+
+temp=$(grep '0018,1310' tmp | cut -c 16-)
+matrix=${temp}
+echo "Matrix:			${matrix}" >> $summary
+
+temp=$(grep '0028,0010' tmp | cut -c 16-)
+row=${temp}
+echo "Row:			${row}" >> $summary
+
+temp=$(grep '0028,0011' tmp | cut -c 16-)
+column=${temp}
+echo "Column:			${column}" >> $summary
+
+temp=$(grep '0028,0030' tmp | cut -c 16-)
+pixelspacing=${temp}
+echo "PixelSpacing:		${pixelspacing}" >> $summary
+
+temp=$(grep '0018,0050' tmp | cut -c 16-)
+slicethickness=${temp}
+echo "SliceThickness:		${slicethickness}" >> $summary
+
+temp=$(grep '0028,0030' tmp | cut -c 16-)
+vox_size=${temp}
+echo "Voxel_size:		${vox_size/\\/ }" >> $summary
+
+temp=$(grep '0018,0050' tmp | cut -c 16-)
+slice_thickness=${temp}
+echo "Slice_thickness:	${slice_thickness}" >> $summary
+
+#temp=$(grep '0020,0037' tmp | cut -c 16-)
+#slice_orientation=${temp}
+echo "Slice_orientation:	N/A" >> $summary
+#
+temp=$(grep '0018,1312' tmp | cut -c 16-)
+slice_encoding=${temp}
+echo "Slice_encoding_dir:	${slice_encoding}" >> $summary
+
+#N/A	       	slice order
+echo "Slice_order:		N/A" >> $summary
+
+temp=$(grep '0018,0088' tmp | cut -c 16-)
+interslice_spacing=${temp}
+echo "Interslice_spacing:	${interslice_spacing}" >> $summary
+
+temp=$(grep '0020,1002' tmp | cut -c 16-)
+number_slice=${temp}
+echo "Number_of_slices:	${number_slice}" >> $summary
+
+temp=$(grep '0018,0083' tmp | cut -c 16-)
+number_echo=${temp}
+echo "Number_of_echoes:	${number_echo}" >> $summary
+
+#N/A	       	shim mode
+echo "Shim_mode:		N/A" >> $summary
+
+temp=$(grep '0019,10a4' tmp | cut -c 16-)
+suppression=${temp}
+echo "FatWater_suppression:	${suppression}" >> $summary
+
+temp=$(grep '0018,0095' tmp | cut -c 16-)
+pixel_bandwidth=${temp}
+echo "Pixel_bandwidth:	${pixel_bandwidth}" >> $summary
+
+temp=$(grep '0020,0105' tmp | cut -c 16-)
+volumes=${temp}
+echo "Volumes:		${volumes}" >> $summary
+
+#rm tmp
+
+exit
+

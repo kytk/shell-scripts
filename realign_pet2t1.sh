7a8829415f88f0ac72f7156d6ca791fe211995e0
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2021-02-16 21:32:16 +0900

    delete temporary files

--- realign_pet2t1.sh
+++ realign_pet2t1.sh
@@ -39,11 +39,11 @@ for f in vol*; do flirt -in $f -ref ${pet}_mr -out ${f%.nii.gz}_r; done
 # Merge realigned frames
 fslmerge -t ${pet}_r vol000?_r.nii.gz
 
-# Delete temporary files
-rm vol*
-
 # Calculate mean of realigned PET images
 fslmaths ${pet}_r -Tmean ${pet}_mean
+
+# Delete temporary files
+rm vol* ${t1}_o.nii.gz ${pet}_[mr].nii.gz ${pet}_mr.nii.gz
 
 echo "Please check the registration of ${t1}_r and ${pet}_mean"
 
